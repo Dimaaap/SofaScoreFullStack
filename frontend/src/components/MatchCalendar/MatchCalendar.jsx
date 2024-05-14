ee63d6a92a2dcc2395b36c1ecb789624d0df37ea
Dimaaap <procdima49@gmail.com>
2024-05-14 09:04:25 +0300

    Add a calendar

--- frontend/src/components/MatchCalendar/MatchCalendar.jsx
+++ frontend/src/components/MatchCalendar/MatchCalendar.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import moment from "moment";
+import "moment/locale/uk";
 import "./MatchCalendar.css"
 
 const MatchCalendar = () => {
+  const [currentDate, setCurrentDate] = useState(moment().locale("uk"));
+
+  const startDay = currentDate.clone().startOf('month').startOf('week');
+  const endDay = currentDate.clone().endOf('month').endOf('week');
+  const days = [];
+  let day = startDay.clone();
+
+  const beforeFirstDayOfMonth = currentDate.clone().startOf("month").day();
+  let totalDaysToShow = beforeFirstDayOfMonth + currentDate.daysInMonth();
+
+  if(totalDaysToShow % 7 !== 0){
+    totalDaysToShow = (Math.floor(totalDaysToShow / 7) + 1) * 7;
+  }
+
+  while (days.length < totalDaysToShow){
+    days.push(day.clone())
+    day.add(1, "day");
+  }
+
+  const renderDaysOfWeek = () => {
+    const dayOfWeek = moment.weekdaysShort(true);
+    return dayOfWeek.map((day) => (
+      <div key={day} className="day-of-week">
+        {day}
+      </div>
+    ))
+  }
+
   return (
-    <div>
-      
+    <div className="calendar-wrapper">
+      <div className="calendar-header">
+        <button className="prev-btn" 
+        onClick={() => setCurrentDate(prev => prev.clone().subtract(1, 'month'))}>
+          &lt;
+        </button>
+        <h2>{currentDate.format('MMMM YYYY')}</h2>
+        <button className="next-btn" 
+        onClick={() => setCurrentDate(prev => prev.clone().add(1, 'month'))}>
+          &gt;
+        </button>
+      </div>
+      <div className="days-of-week">
+        {renderDaysOfWeek()}
+      </div>
+      <div className="calendar-body">
+        {days.map(dayItem => (
+            <div
+              key={dayItem.format('DDMMYYYY')}
+              className={`day ${dayItem.month() === currentDate.month() ? 'current-month' : 'other-month'}`}
+            >
+              {dayItem.month() === currentDate.month() ? dayItem.format('D') : ''}
+            </div>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default MatchCalendar
--- frontend/src/pages/Footbal/Football.jsx
+++ frontend/src/pages/Footbal/Football.jsx
@@ -2,28 +2,33 @@ import React from 'react'
 import "./Football.css"
 import TopHeader from '../../components/TopHeader/TopHeader'
 import BottomHeader from "../../components/BottomHeader/BottomHeader"
+import MatchCalendar from '../../components/MatchCalendar/MatchCalendar'
 
 const Football = () => {
   return (
     <>
         <TopHeader />
         <BottomHeader />
-        <small>
-            <b>
-                Рахунок футбольних матчів у реальному часі
-                і розклад на сьогодні
-            </b>
-        </small>
-        <div class="container" id="main-container">
-            <div className="page-content">
-                <div className="section left-page">
-                    Left Section
-                </div>
-                <div className="section center-page">
-                    Center Section
-                </div>
-                <div className="section right-page" id="right-page">
-                    Right Section
+        <div className="body">
+            <small>
+                <b>
+                    Рахунок футбольних матчів у реальному часі
+                    і розклад на сьогодні
+                </b>
+            </small>
+            <div class="container" id="main-container">
+                <div className="page-content">
+                    <div className="section left-page" id="left-section">
+                        <div className="calendar-section">
+                            <MatchCalendar />
+                        </div>
+                    </div>
+                    <div className="section center-page">
+                        Center Section
+                    </div>
+                    <div className="section right-page" id="right-page">
+                        Right Section
+                    </div>
                 </div>
             </div>
         </div>
