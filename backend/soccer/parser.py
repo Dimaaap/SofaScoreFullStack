a6808c1a84508b58251cb8182527448f5b2b307f
Dimaaap <procdima49@gmail.com>
2024-06-05 23:54:07 +0300

    Parse cities in each country

--- backend/soccer/parser.py
+++ backend/soccer/parser.py
@@ -2,7 +2,7 @@ import aiohttp
 import asyncio
 import json
 
-import bs4
+
 import requests
 from typing import Mapping, Iterable, Union
 from bs4 import BeautifulSoup
@@ -171,11 +171,40 @@ class ParseCities:
                     full_link = self.__form_full_page_link(cities_list_link["href"])
                     title = cities_list_link["title"]
                     country_title = self.get_country_title_from_full_link_name(title)
-                    print(country_title)
+                    countries_page_href[country_title] = full_link
+        self.get_all_countries_cities_list(countries_page_href)
+
+    def get_all_countries_cities_list(self, countries_href_dict: Mapping[str, str]):
+        country_cities_dict = {}
+        for country, href in countries_href_dict.items():
+            res = requests.get(href).text
+            soup = BeautifulSoup(res, "lxml")
+            cities_list = self.__parse_cities_table(soup)
+            country_cities_dict[country] = cities_list
+        print(country_cities_dict)
+
+    def __parse_cities_table(self, soup: BeautifulSoup):
+        cities_list = []
+        city_table = soup.find("table", {"class": "sortable"})
+        try:
+            table_body = city_table.find("tbody")
+            city_rows = table_body.find_all("tr")
+        except AttributeError:
+            return []
+        for city in city_rows:
+            try:
+                city_cell = city.find("td").text
+            except AttributeError:
+                city_cell = ""
+            print(city_cell)
+        return cities_list
+
+
+
 
     def get_country_title_from_full_link_name(self, link_name: str):
-        banned_country = ("Somaliland", "South Sudan", "Svalbard", "Transinstria", "Vatican City", "Western Sahara",
-                          "South Ossetia")
+        banned_country = ("Somaliland", "South Sudan", "Svalbard", "Transnistria", "Vatican City", "Western Sahara",
+                          "South Ossetia", "Pitcairn Islands")
         link_name_list = link_name.split()
         for i in link_name_list:
             if i == "in":
@@ -185,8 +214,24 @@ class ParseCities:
             result_string = "".join(result_string.split()[0])
         if result_string in banned_country:
             result_string = ""
-        return result_string
+        return self.__additional_cities_list_clean(result_string)
 
+    def __additional_cities_list_clean(self, city_string):
+        city_list = city_string.split()
+        need_clean_strings = {
+            "Australia": "Australia", "Bolivian": "Bolivia",
+            "Cameroon": "Cameroon", "Denmark": "Denmark", "France": "France",
+            "Georgia": "Georgia", "Kuwait": "Kuwait", "Liechtenstein": "Liechtenstein",
+            "Micronesia": "Micronesia", "Puerto": "Puerto Rico", "Marino": "San Marino", "Spain": "Spain",
+            "Nicaragua": "Nicaragua", "Guadeloupe": "Guadeloupe"
+        }
+        for i in need_clean_strings.keys():
+            if i in city_list:
+                city_list = need_clean_strings[i]
+                return str(city_list)
+        if city_string == "Portal:Cities":
+            return ""
+        return ' '.join(city_list)
 
     def __form_full_page_link(self, short_link: str):
         return f"https://en.m.wikipedia.org{short_link}"
